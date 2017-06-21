da6dc97cbe7b74efc20421dc7b698f14f0b90806
EamonBachari <eamonbachari@gmail.com>
2017-06-21 09:46:27 -0500

    Incorporate data file.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+const data = require('./data.js');
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, resp){
+
+  resp.render('index', {users :data.users})
+
+});
+
+
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server launch successful.")
+});
